b8278d8a150baf89bfb7b09578e3ce2c7b68424d
sheila barron <sheilabarron@yahoo.com>
2019-03-03 02:58:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  console.log("Happy holidays, ${name}!")
+  return("Happy holidays, ${name}!")
 }
 
 happyHolidaysTo("you");
 
 function happyHolidaysTo(holiday, name) {
-  console.log(`Happy ${holiday}, ${name}!`)
+  return(`Happy ${holiday}, ${name}!`)
 }
 
 happyHolidaysTo("Mother\'s Day", "you");
 
 function holidayCountdown(days, holiday) {
-  console.log("It\'s ${days} until ${holiday}!")
+  return("It\'s ${days} until ${holiday}!")
 }
 
 holidayCountdown(20, "Mother\'s Day");
