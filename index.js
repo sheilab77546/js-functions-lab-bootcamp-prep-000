8593b296f1074c40c80ef562ab181d8898b20016
sheila barron <sheilabarron@yahoo.com>
2019-03-03 02:50:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // write your code below!
-function happyHolidays() {
-  return ('Happy holidays!')
-}
+
 
 var name = "you"
 
@@ -9,11 +7,15 @@ var holiday = "Mother's Day"
 
 var days = 20
 
+function happyHolidays() {
+  return ('Happy holidays!')
+}
+
 function happyHolidaysTo(name) {
   return console.log("Happy holidays, ${name}!")
 }
 
-happyHolidaysTo(name)
+happyHolidaysTo(you)
 
 function happyHolidaysTo(holiday, name) {
   return console.log(`Happy ${holiday}, ${name}!`)
