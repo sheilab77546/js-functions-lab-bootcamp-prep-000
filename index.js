c17de244f84d7be85f47735aa44cd85505bf85bc
sheila barron <sheilabarron@yahoo.com>
2019-03-03 03:01:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 // write your code below!
 
-
-var name = "you"
-
-var holiday = "Mother's Day"
-
-var days = 20
-
 function happyHolidays() {
-  return ("Happy holidays!")
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
-  return("Happy holidays, ${name}!")
+  return "Happy holidays, ${name}!"
 }
 
 happyHolidaysTo("you");
 
 function happyHolidaysTo(holiday, name) {
-  return(`Happy ${holiday}, ${name}!`)
+  return `Happy ${holiday}, ${name}!`
 }
 
 happyHolidaysTo("Mother\'s Day", "you");
 
 function holidayCountdown(days, holiday) {
-  return("It\'s ${days} until ${holiday}!")
+  return "It\'s ${days} days until ${holiday}!"
 }
 
 holidayCountdown(20, "Mother\'s Day");
