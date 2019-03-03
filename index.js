ac0fcf6066528ec059c1472e813aad05560589db
sheila barron <sheilabarron@yahoo.com>
2019-03-03 02:38:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ var holiday = "holidays"
 var days = 30
 
 function happyHolidaysTo(name) {
-  return console.log (`Happy holidays, ${name}!`)
+  return console.log(`Happy holidays, ${name}!`)
 }
 
 function happyHolidaysTo(holiday, name) {
-  return console.log (`Happy ${holiday}, ${name}!`)
+  return console.log("Happy ${holiday}, ${name}!")
 }
 
 function holidayCountdown(days, holiday) {
-  return console.log (`It\'s ${days} until ${holiday})!`
-}
-
-}
+  return console.log("It\'s ${days} until ${holiday}!")
 }
